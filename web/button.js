eb094919e07be3696e52a3831cc0cc95ef1b30e3
TheWebTuber <atomkhanthak@gmail.com>
2025-01-10 02:59:53 +0100

    game 1

---
+++ web/button.js
@@ -0,0 +1,12 @@
+
+function togglePopup() {
+    const popup = document.getElementById("popup-1");
+    popup.classList.toggle("active"); // Toggle active state
+  }
+
+  
+function togglePopupoverons() {
+  const popup = document.getElementById("popup-2");
+  popup.classList.toggle("active"); // Toggle active state
+}
+
