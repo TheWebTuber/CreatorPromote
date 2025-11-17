b7bdc0840220ebca451720aaeb426336569282ee
TheWebTuber <atomkhanthak@gmail.com>
2025-11-17 10:48:22 +0100

    Delete HVA/kobuki2.0 directory

--- HVA/kobuki2.0/js/cams.js
+++
@@ -1,6 +0,0 @@
-function toggleStats() {
-  const content = document.querySelector('.stats-content');
-  const button = document.querySelector('.toggle-stats');
-  content.classList.toggle('show');
-  button.classList.toggle('open');
-}
